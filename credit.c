b5b5c3c8b2f3c79a398b753de1f6f8c5aa3b4926
Chance <chancej715@gmail.com>
2022-09-28 18:33:01 -0500

    Takes number input and stores each digit in array.

--- credit.c
+++ credit.c
@@ -1,17 +1,17 @@
 /**
  * Credit card number validation program.
- * 
+ *
  * Written by Chance Johnson 2022-09-28
- * 
- * This program uses Luhn's Algorithm to determine the syntactical validity of a credit 
+ *
+ * This program uses Luhn's Algorithm to determine the syntactical validity of a credit
  * card number.
- * 
+ *
  * The following steps are taken on the given card number:
- *  1. Multiply every other digit by 2, starting with the number's second-to-last digit. 
+ *  1. Multiply every other digit by 2, starting with the number's second-to-last digit.
  *  Add those products' digits together.
- * 
+ *
  *  2. Add the sum to the sum of the digits that weren't multiplied by 2.
- * 
+ *
  *  3. If total modulo 10 is congruent with 0, the number is valid!
  * */
 
@@ -24,7 +24,7 @@ int main(void)
     unsigned long cardNumber = 0;
 
     /* A valid credit card number will not be longer than 16 digits */
-    while(cardNumber < 1 || cardNumber > 9999999999999999)
+    while (cardNumber < 1 || cardNumber > 9999999999999999)
     {
         printf("Number: ");
         scanf("%lu", &cardNumber);
@@ -32,4 +32,22 @@ int main(void)
         /* Clear buffer to prevent endless loops upon entering certain characters, like letters */
         while ((getchar()) != '\n');
     }
+
+    /**
+     * STEP 1
+     * */
+    int cardNumberArr[17];
+    int lastDigit;
+    int counter = 0;
+
+    do
+    {
+        /* Store card number digits into array */
+        lastDigit = cardNumber % 10;
+        cardNumberArr[counter] = lastDigit;
+        cardNumber /= 10;
+        counter++;
+    } while (cardNumber > 0);
+
+    return 0;
 }
